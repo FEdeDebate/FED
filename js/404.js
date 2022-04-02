95c610083ad981b87a0017cbd37331cf821548ec
marvil-exe <101852957+FEdeDebate@users.noreply.github.com>
2022-04-01 22:26:05 -0600

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,4 +1,4 @@
-var usernames = ["A01723738", "A01723546", "A01723803", "A01723810"]; //list of people on which we have content
+var usernames = ["A01723738", "A01723546", "A01723803", "A01723810"]; //list of people whose results we have
 
 function start()
 {
