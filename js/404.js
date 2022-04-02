afe358e991917b12a8da9fafaf8dd074eb36d775
marvil-exe <101852957+FEdeDebate@users.noreply.github.com>
2022-04-01 22:24:20 -0600

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -5,8 +5,4 @@ function start()
     url = document.URL
     studentID = url.substring(url.lastIndexOf("/") + 1);
     document.getElementById("comprobar").innerHTML = "Escribiste tu matricula bien? '" + studentID + "'"
-    if (usernames.includes(studentID))
-    {
-        window.location.href += studentID + ".pdf";
-    }
 }
