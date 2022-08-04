2b2436996cc003aa910af2b986b272e6e3608514
Jorge Martínez <6marvil@gmail.com>
2022-08-04 07:51:06 -0500

    added 404.html

---
+++ js/404.js
@@ -0,0 +1,12 @@
+var usernames = ["A01723738", "A01723546", "A01723803", "A01723810"]; //list of people whose results we have
+
+function start()
+{
+    url = document.URL
+    studentID = url.substring(url.lastIndexOf("/") + 1);
+    document.getElementById("comprobar").innerHTML = "Escribiste tu matricula bien? '" + studentID + "'"
+    if (usernames.includes(studentID))
+    {
+        window.location.href += ".pdf";
+    }
+}
