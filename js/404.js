db0579bd0cd9e42ccaf830e66ce5dd065eb3a00f
Jorge Martínez <6marvil@gmail.com>
2022-08-04 09:29:15 -0500

    added results page

--- js/404.js
+++ js/404.js
@@ -1,12 +1,11 @@
 var usernames = ["A01723738", "A01723546", "A01723803", "A01723810"]; //list of people whose results we have
 
-function start()
+function start(typeOfFail)
 {
     url = document.URL
-    studentID = url.substring(url.lastIndexOf("/") + 1);
-    document.getElementById("comprobar").innerHTML = "Escribiste tu matricula bien? '" + studentID + "'"
-    if (usernames.includes(studentID))
-    {
-        window.location.href += ".pdf";
-    }
+    urlEnding = url.substring(url.lastIndexOf("/") + 1);
+    if (typeOfFail == "year") document.getElementById("comprobar").innerHTML = "Escribiste el año bien? '" + urlEnding + "'"
+    else if (typeOfFail == "id") document.getElementById("comprobar").innerHTML = "Escribiste tu matricula bien? '" + urlEnding + "'"
+    else document.getElementById("comprobar").innerHTML = "Escribiste el URL bien?"
+    if (usernames.includes(urlEnding)) { window.location.href += ".pdf"; }
 }
