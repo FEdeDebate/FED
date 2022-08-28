c5df1481bfea042c5ce81162ad37904e7155646d
Jorge Martínez <6marvil@gmail.com>
2022-08-27 23:17:09 -0500

    Comment results.js file, fix base tag in result's index.html

--- js/results.js
+++ js/results.js
@@ -1,77 +1,54 @@
-var srch = document.getElementById("search");
-srch.addEventListener("keydown", function (e) {
-    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
-        redirect();
-    }
-});
+var usernames = [
+    "A01723738", // Martina Rodríguez
+    "A01723546", // Carolina Mercado González
+    "A01723803", // Jorge Luis Martínez Villarreal
+    "A01723810" // Sebastián Rodríguez Ortiz
+]; 
 
-var alertExists = false;
-var close;
-var i;
-var usernames = ["A01723738", "A01723546", "A01723803", "A01723810"]; //list of people on which we have content
+var errorMessage = document.getElementById("errorMessage");
+document.getElementById("search").addEventListener("keydown", function (e) { if (e.code === "Enter") redirect(); } );
 
 function redirect() {
-
-    var errorMessage = document.getElementById("errorMessage");
 
     const studentID = document.querySelector('input').value.replace(/[^A-z0-9]/gi, '').toUpperCase(); //get the string of text after the last / in the URL
     var similarities = []; //array of percentage of similarities between list of students and entered student
 
-    for (let i = 0; i < usernames.length; i++) //for every student we have, run the similarity calculation, and push it to the similarities array
-    {
-        var perc=Math.round(similarity(studentID,usernames[i])*10000)/100;
-        similarities.push(perc)
+
+    // For every student we have, run the similarity calculation, and push it to the similarities array
+    for (let i = 0; i < usernames.length; i++) {
+        var perc=Math.round(similarity(studentID,usernames[i])*10000)/100; //calculate the percentage of similarity between the entered student and the current student
+        similarities.push(perc) //push the percentage to the similarities array
     }
     var max = Math.max(...similarities) //get the value of the highest similarity
     var index = similarities.indexOf(max) //get the position of the highest similarity, which equals the position of the person whose ID is closest
-    var logging = "similarities: " + similarities + "\nmax: " + max + "\nindex: " + index;
-    console.log(logging);
 
 
-    if (usernames.includes(studentID)) //if the ID entered matches the ID with the highest similarity, then the user entered their ID correctly, and they can be diredected to their result
-    {
-        window.location.href += "2022/" + studentID + ".pdf";
-    }
-    else if ((max >= 75)) //if the user didn't enter their ID correctly, but there's an ID that is 75% or more similar, they can be redirected to their result if they decide to
-    {
-        document.querySelector('input').select()
-        errorMessage.innerHTML = "Ups! La matricula que escribiste no está en nuestro sistema. Querías decir <a href=\"" + window.location.href + "2022/" + usernames[index] + ".pdf\">" + usernames[index] + "?";
-        makeErrorMessageVisible();
-    }
-    else if (studentID == "")
-    {
-        errorMessage.innerHTML = "Porfavor escribe tu matrícula.";
-        makeErrorMessageVisible();
-    }
-    else
-    {
-        errorMessage.innerHTML = "Ups! No encontramos tu matrícula. Intenta de nuevo.";
-        makeErrorMessageVisible();
-    }
+    // If the ID entered matches one of the IDs in the list, redirect to the results page of that person
+    if (usernames.includes(studentID)) window.location.href += "2022/" + studentID + ".pdf";
+
+    // If the user didn't enter their ID correctly, but there's an ID that is 75% or more similar, they can be redirected to their result if they decide to.
+    else if ((max >= 75)) displayErrorMessage(
+        "Ups! La matricula que escribiste no está en nuestro sistema. Querías decir <a href=\"" + window.location.href + "2022/" + usernames[index] + ".pdf\">" + usernames[index] + "?");
+    // If the user didn't enter anything into the search bar, ask them to enter their ID.
+    else if (studentID == "") displayErrorMessage("Porfavor escribe tu matrícula.");
+    // If the user entered an ID that is not in the list, tell them to try again.
+    else displayErrorMessage("Ups! No encontramos tu matrícula. Intenta de nuevo.");
 }
 
-function makeErrorMessageVisible() {
-    var errorMessage = document.getElementById("errorMessage");
+function displayErrorMessage(text) {
+    errorMessage.innerHTML = text;
     errorMessage.style.color = "#C45886";
     errorMessage.style.backgroundColor = "#3D202F";
     errorMessage.style.border = "8px solid #3D202F";
-
 }
 
 function similarity(string1, string2)
 {
     var longer = string1;
     var shorter = string2;
-    if (string1.length < string2.length)
-    {
-        longer = string2;
-        shorter = string1;
-    }
+    if (string1.length < string2.length) longer = string2; shorter = string1;
     var longerLength = longer.length;
-    if (longerLength === 0)
-    {
-        return 1.0;
-    }
+    if (longerLength === 0) return 1.0;
     return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
 }
 
@@ -89,24 +66,12 @@ function editDistance(string1, string2)
             if (i == 0)
             costs[j] = j;
             else
-            {
-                if (j > 0)
-                {
+            { if (j > 0) {
                     var newValue = costs[j - 1];
                     if (string1.charAt(i - 1) != string2.charAt(j - 1))
-                    newValue = Math.min(Math.min(newValue, lastValue),
-                        costs[j]) + 1;
+                    newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
                     costs[j - 1] = lastValue;
-                    lastValue = newValue;
-                }
-            }
-        }
-        if (i > 0)
-            costs[string2.length] = lastValue;
-    }
-    return costs[string2.length];
-}
-
-function gotolink(url) {
-    window.open(url)
+                    lastValue = newValue; } }
+        } if (i > 0) costs[string2.length] = lastValue;
+    } return costs[string2.length];
 }
