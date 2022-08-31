ca6225bd57da300a91033ced0b9c21d2f5a10f6a
Jorge Martínez <6marvil@gmail.com>
2022-08-31 12:07:59 -0500

    move data to be changed to a separate file

--- js/results.js
+++ js/results.js
@@ -1,23 +1,10 @@
-// things to change
-const data = 
-{
-    "fed1" : [
-        { "ID" : "A01723738", "Name" : "Martina Rodríguez" },
-        { "ID" : "A01723546", "Name" : "Carolina Mercado González" },
-        { "ID" : "A01723803", "Name" : "Jorge Luis Martínez Villarreal" },
-        { "ID" : "A01723810", "Name" : "Sebastián Rodríguez Ortiz" }
-    ]
-}
-var version = "fed1/" //the version of the event that is being searched
-var usernames = data[version.slice(0, -1)]; //
-
-
 //
 //
 // DO NOT CHANGE
 //
 //
 
+var usernames = data[version.slice(0, -1)]; //
 
 var errorMessage = document.getElementById("errorMessage");
 document.getElementById("search").addEventListener("keydown", function (e) { if (e.code === "Enter") redirect(); } );
